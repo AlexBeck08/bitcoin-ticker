6827d5744f22af3139a3aad6ca27d61efa33625d
Alex Beck <alexbeck03@gmail.com>
2019-01-23 17:19:13 -0600

    add https requests from API

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
 
-app.use(bodyParser.uslencoded({
+app.use(bodyParser.urlencoded({
    extended: true
 }));
 
@@ -11,6 +12,24 @@ app.get('/', function(req, res) {
    res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/', function(req, res) {
+   // console.log(req.body.crypto);
+   // console.log(req.body.fiat);
+   console.log(req);
+   var crypto = req.body.crypto;
+   var fiat = req.body.fiat;
+   var price = request('https://apiv2.bitcoinaverage.com/indices/global/ticker/' + crypto + fiat, function(error, response, body) {
+      var data = JSON.parse(body);
+      var price = data.last;
+      var currentDate = data.display_timestamp;
+
+      res.write('<p>The current date is ' + currentDate + '</p>');
+
+      res.write('<h1>The price of ' + crypto + ' is ' + price + ' ' + fiat + '.</h1>');
+
+      res.send();
+   });
+});
 
 
 
