39a43fe5a750738771847c6663722d9d48b66ae1
Alex Beck <alexbeck03@gmail.com>
2019-01-22 18:10:33 -0600

    initial commit. Set up root route.

---
+++ index.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.uslencoded({
+   extended: true
+}));
+
+app.get('/', function(req, res) {
+   res.sendFile(__dirname + '/index.html');
+});
+
+
+
+
+app.listen(3000, function() {
+   console.log('Bitcoin-Ticker is Ticking');
+});
